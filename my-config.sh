b14ac636c81375297cc147860a4c0b88dca16494
amekusa <amekusa@users.noreply.github.com>
2022-04-17 16:35:22 +0900

    Install required libraries in my-config.sh

--- my-config.sh
+++ my-config.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+echo
+echo "---- Libraries --------"
+
+brew install \
+freetype \
+fdk-aac \
+libass \
+libtool \
+libogg \
+libvorbis \
+opus \
+sdl2 \
+shtool \
+texi2html \
+x264 \
+x265 \
+nasm
+
+echo
+echo "---- Configuration --------"
+
 PREFIX="$HOMEBREW_PREFIX"
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOMEBREW_PREFIX/lib
 
@@ -8,6 +29,7 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOMEBREW_PREFIX/lib
 --enable-gpl \
 --enable-nonfree \
 --enable-libass \
+--enable-libfreetype \
 --enable-libfdk-aac \
 --enable-libfreetype \
 --enable-libvorbis \
@@ -15,3 +37,8 @@ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOMEBREW_PREFIX/lib
 --enable-libx265 \
 --enable-libopus \
 --enable-libwebp
+
+echo
+echo "Configuration Done."
+echo
+echo "Run 'make' to build."
